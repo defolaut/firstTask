2bdea66068a7c31e83e662d880f6b427c36f04e6
defolaut <repetformat18@gmail.com>
2020-03-19 16:08:01 +0400

    few classes added

---
+++ src/main/java/network/IPAdress.java
@@ -0,0 +1,13 @@
+package network;
+
+public class IPAdres {
+    private String ipAdres;
+
+    public IPAdres(String ipAdres) {
+        this.ipAdres = ipAdres;
+    }
+
+    public String getIpAdres() {
+        return ipAdres;
+    }
+}
---
+++ src/main/java/routeProvider/RouteNotFoundException.java
@@ -0,0 +1,4 @@
+package routeProvider;
+
+public class RouteNotFoundException {
+}
